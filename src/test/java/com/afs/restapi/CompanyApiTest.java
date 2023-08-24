5bafdd296dffefd88673c6fc5710591a9f45aa25
ninajairacagomoc <jairalael101@gmail.com>
2023-08-25 07:23:34 +0800

    feat: implemented should_find_companies_by_page

--- src/test/java/com/afs/restapi/CompanyApiTest.java
+++ src/test/java/com/afs/restapi/CompanyApiTest.java
@@ -109,18 +109,18 @@ class CompanyApiTest {
         Company company1 = getCompany1();
         Company company2 = getCompany2();
         Company company3 = getCompany3();
-        inMemoryCompanyRepository.insert(company1);
-        inMemoryCompanyRepository.insert(company2);
-        inMemoryCompanyRepository.insert(company3);
+        Company savedCompany1 = companyJpaRepository.save(company1);
+        Company savedCompany2 = companyJpaRepository.save(company2);
+        Company savedCompany3 = companyJpaRepository.save(company3);
 
         mockMvc.perform(get("/companies")
                         .param("pageNumber", "1")
                         .param("pageSize", "2"))
                 .andExpect(MockMvcResultMatchers.status().is(200))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(savedCompany1.getId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(company1.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(savedCompany2.getId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value(company2.getName()))
         ;
     }
